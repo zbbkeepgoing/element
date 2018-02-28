56fcaaa35d9a36890d94130a3fd82f6f28ef0b63
luguosheng1314 <550175214@qq.com>
2018-02-28 18:43:03 +0800

    var change

--- src/index.js
+++ src/index.js
@@ -69,8 +69,8 @@ import Header from '../packages/header/index.js';
 import Aside from '../packages/aside/index.js';
 import Main from '../packages/main/index.js';
 import Footer from '../packages/footer/index.js';
-import locale from 'element-ui/src/locale';
-import CollapseTransition from 'element-ui/src/transitions/collapse-transition';
+import locale from 'kyligence-ui/src/locale';
+import CollapseTransition from 'kyligence-ui/src/transitions/collapse-transition';
 
 const components = [
   Pagination,
@@ -171,7 +171,7 @@ if (typeof window !== 'undefined' && window.Vue) {
 }
 
 module.exports = {
-  version: '1.0.0',
+  version: '1.0.1',
   locale: locale.use,
   i18n: locale.i18n,
   install,
